15d60dea54c6bf943d4d96451862e28342d9eac3
Jonathan Moser <jonathancmoser@gmail.com>
2022-02-01 19:04:04 -0500

    set up html

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,8 @@
+const router = require('express').Router();
+const { notes } = require('../../data/db.json');
 // set up GET
 
 // set up POST
 
 // set up bonus
+module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,3 +1,16 @@
+const path = require('path');
+const router = require('express').Router();
+
+
 // get return notes
 
-// get * to return index
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+// Get * for notes.html
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
